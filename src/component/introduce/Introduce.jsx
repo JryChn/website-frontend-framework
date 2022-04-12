dd560338e3072f1aa0f6c45f7c380d8aaadfea1a
Jeremy Chen <jeremychen@djeremychen.com>
2022-04-12 23:47:28 +0800

    improve introduce

--- src/component/introduce/Introduce.jsx
+++ src/component/introduce/Introduce.jsx
@@ -4,7 +4,13 @@ import style from "./Introduce.module.scss";
 export default class Introduce extends Component {
   render() {
     return (
-      <div className={style.container} id="introduce">
+      <div
+        className={style.container}
+        id="introduce"
+        style={{
+          backgroundImage: `url(${require("../../asset/background.jpg")})`,
+        }}
+      >
         <div>
           <h4>{this.props.introduce.subTitle}</h4>
           <h1>{document.title + ".com"}</h1>
@@ -12,9 +18,6 @@ export default class Introduce extends Component {
         </div>
         <div>
           <img src={this.props.introduce.mainImage} alt=""></img>
-        </div>
-        <div>
-          <img src={this.props.introduce.subImage} alt=""></img>
         </div>
         <div>
           <h6>{'"' + this.props.introduce.quote.content + '"'}</h6>
