ed3ca0c6a444e677bdc59a9b4a50c29c1fd19989
Jeremy Chen <jeremychen@djeremychen.com>
2023-06-26 00:12:54 +0800

    update new frame

---
+++ src/component/aboutme.rs
@@ -0,0 +1,12 @@
+use dioxus::prelude::*;
+
+pub fn AboutMe(cx:Scope) -> Element{
+    cx.render(rsx!{
+        div{
+            // display:"block",
+            // border:"1px red solid",
+            // width:"100%",
+            // height:"100vh",
+        }
+    })
+}
---
+++ src/component/mod.rs
@@ -0,0 +1,7 @@
+pub mod welcome;
+pub mod aboutme;
+pub mod article;
+pub mod footer;
+pub mod gallery;
+pub mod story;
+pub mod header;
