ac01fc6cda1980c9fe5fb9069f867566b25f249a
Jeremy Chen <jeremychen@djeremychen.com>
2024-03-28 18:06:51 +0800

    implement aboutme page

--- src/component/aboutMeContent/experience.rs
+++ src/component/aboutMeContent/experience.rs
@@ -1,14 +1,16 @@
 use dioxus::prelude::*;
 
+use crate::component::aboutMeContent::ExperienceContent;
+
 #[component]
-pub fn Experience() ->Element{
-    let mut hidden_description = use_signal(||false);
-    let hidden_description_css = if hidden_description(){
-        (" h-0 "," rotate-0 ")
-    }else{
+pub fn Experience(experiences: Vec<ExperienceContent>) -> Element {
+    let mut hidden_description = use_signal(|| false);
+    let hidden_description_css = if hidden_description() {
+        (" h-0 ", " rotate-0 ")
+    } else {
         (" ", " rotate-90 ")
     };
-    rsx!{
+    rsx! {
         div { class: "bg-[rgb(195,201,195)] flex flex-col select-none cursor-default",
             div{class:"w-screen h-[500px] md:h-56"}
             div{
@@ -21,16 +23,16 @@ pub fn Experience() ->Element{
             div{
                 class:"w-[90%] mx-auto border-b border-black mt-16 md:w-5/6 md:mt-32 "
             }
-            div{ 
+            div{
                class:"w-3/4 mx-auto mb-64" ,
-                //add iterators here
+                for exp in experiences{
                 li{
                     class:"border-gray-600 border-b w-full py-10 flex flex-col my-16",
                     div{
                         class:"h-1/5 flex flex-row font-normal text-sm",
                         div{
                             class:"w-1/4 flex items-center justify-start",
-                            "Since 2016"
+                            "{exp.start_time}"
                         }
                         div{
                             class:"w-1/4 flex items-center justify-center",
@@ -40,29 +42,27 @@ pub fn Experience() ->Element{
                                     hidden_description.set(!hidden_description());
                                 }
                             }
-                            "the creeps title problem"
+                            "{exp.title}"
                         }
                         ul{
                             class:"flex-1 flex justify-end items-center",
-                            // for here 
+                                for key in exp.keywords{
                             li{
                             class:"border border-[rgb(20,89,41)] rounded-2xl px-4 py-0.5 mx-1 hover:bg-[rgb(20,89,41)]",
-                            "keywords"
+                            "{key}"
                             }
-                            li{
-                            class:"border border-[rgb(20,89,41)] rounded-2xl px-4 py-0.5 mx-1 hover:bg-[rgb(20,89,41)]",
-                            "keywords"
-                            }
+                                }
                         }
                     }
                     div{
                         class:"flex items-start justify-center mt-10 group/description overflow-hidden duration-700 {hidden_description_css.0}",
                         div{
                             class:"w-5/6 text-sm font-light",
-                            "set to true to allow word to be drawn partly outside of the canvas.// Allow word bigger than the size of the canvas to be drawnThis is an upgraded form of [use_future] with an integrated channel system. Specifically, the coroutine generated here comes with an [UnboundedChannel] built into it - saving you the hassle of building your own.
-This makes it effective for apps that need to interact with an event loop or some asynchronous code without thinking too hard about state."
+                            "{exp.description}"
                         }
                     }
+                }
+
                 }
             }
         }
--- src/component/aboutMeContent/skill.rs
+++ src/component/aboutMeContent/skill.rs
@@ -1,7 +1,23 @@
+use charming::{Chart, val};
+use charming::component::{RadarCoordinate, RadarIndicator, Title};
+use charming::series::Radar;
+use charming::theme::Theme;
 use dioxus::prelude::*;
 
+use crate::component::aboutMeContent::SkillContent;
+
+struct Skills{
+    title:String,
+    description:String,
+    chart : Chart
+}
+
 #[component]
-pub fn Skill() ->Element{
+pub fn Skill(skill_content: Vec<SkillContent>) ->Element{
+    let mut charts = Vec::new();
+    for skill in skill_content{
+        charts.push(create_radar(&skill));
+    }
     rsx!{
         div { class: "relative bg-transparent -top-48 select-none cursor-default",
             div { class: "hidden text-5xl font-medium m-12 md:inline-block", "技能和技巧" }
@@ -9,19 +25,24 @@ pub fn Skill() ->Element{
                 // small screen render
                 div{
                     class:"flex flex-col item-center md:hidden",
-                    // add for here
+                    for chart in charts{
                     div{
                        class:"w-screen my-16 flex flex-col",
-                    div { class: "border-gray-950 border-2 w-72 h-72 mx-auto"}
+                    div { class: "border-gray-950 border-2 w-72 h-72 mx-auto",
+
+                            }
                 div { class: "flex flex-col my-8 items-center",
                     div { class: "text-4xl font-normal text-white",
-                        "Skill Title"
+                        "{chart.title}"
                     }
                     div { class: "w-96 text-lg text-white font-light my-4",
-                        "Skill Description, descripte this skill is for what and the meaning of it, it should be long setence or just a few word."
+                        "{chart.description}"
                     }
                 }
                     }
+
+                    }
+                    // add for here
                 }
                 // big screen render
                 div{
@@ -45,5 +66,27 @@ pub fn Skill() ->Element{
                 }
             }
         }
+    }
+}
+
+fn create_radar(skill: &SkillContent)->Skills{
+    let mut indicators  = Vec::new();
+    let mut actual_values = Vec::new();
+    for r in skill.skill_content{
+        let indicator = RadarIndicator::new().name(r.name).min(0).max(10);
+        indicators.push(indicator);
+        // filter values
+        let mut value = r.value as f64;
+        while value >10f64{
+            value = value/10f64;
+        }
+        actual_values.push(value);
+    }
+     Skills{
+        title:skill.clone().skill_name,
+        description: skill.clone().description,
+        chart:
+        Chart::new().title(Title::new().text(skill.clone().skill_name)).radar(RadarCoordinate::new().indicator(indicators
+        )).series(Radar::new().data(Vec::from(actual_values)))
     }
 }
