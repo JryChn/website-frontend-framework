dc4056c5b3a1d9151cc4d068fd50b29443bd3a5e
Jeremy Chen <jeremychen@djeremychen.com>
2024-04-06 19:18:53 +0800

    common fix

--- src/component/articleList/keywords.rs
+++ src/component/articleList/keywords.rs
@@ -10,13 +10,16 @@ use crate::utils::wordCloud::word_cloud_maker;
 #[component]
 pub fn Keywords(keywords: Signal<HashMap<String, i32>>) -> Element {
     let keywords = keywords();
-    eval(&word_cloud_maker(&keywords));
     rsx! {
         div {
             id: "article_list_sidebar_key_words",
             class: "w-11/12 h-[20vw] mx-auto my-10 flex-1",
             img { class: "inline-block w-8 h-8 my-2 mr-[2%]", src: mg!(file("src/assets/svg/keywords.svg")) }
-            div { id: "article_list_keys", class: "w-11/12 h-[90%]" }
+            div { id: "article_list_keys",
+                onmounted:move|_|{
+                    eval(&word_cloud_maker(&keywords));
+                },
+                class: "w-11/12 h-[90%]" }
         }
     }
 }
